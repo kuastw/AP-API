177da2d99356022c5b508426412d29c52504a1a0
registerAutumn <a95011a95011@gmail.com>
2014-06-23 03:41:37 +0800

    Testing
    Maybe Clean more (X

--- function.py
+++ function.py
@@ -4,7 +4,7 @@ import time
 import hashlib
 import requests
 from lxml import etree
-
+from bs4 import BeautifulSoup as bs
 
 RANDOM_ID = "AG009"
 
@@ -30,9 +30,12 @@ def login(username, password):
     payload = {"uid": username, "pwd": password}
     r = s.post(login_url, data=payload)
 
+    is_login = 'f_index.html' in r.content
 
+    """
     root = etree.HTML(r.text)
     is_login = not root.xpath("//script")[-1].text.startswith("alert")
+    """
 
     if is_login:
         hash_value = hash(username)
@@ -51,6 +54,9 @@ def is_login(hash_value):
 
 
 def query(hash_value, username=None, password=None, qid=None, args=None):
+    payload = random_number(hash_value, RANDOM_ID)
+
+    """
     ls_random = random_number(hash_value, RANDOM_ID)
     payload = {"arg01": "", "arg02": "", "arg03": "",
                 "fncid": "", "uid": "", "ls_randnum": ""}
@@ -60,7 +66,7 @@ def query(hash_value, username=None, password=None, qid=None, args=None):
     payload["arg01"] = args["arg01"]
     payload["arg02"] = args["arg02"]
     payload["arg03"] = username
-
+    """
     r = sd[hash_value].post(query_url % (qid[:2], qid), data=payload)
 
     return r.content
@@ -69,6 +75,7 @@ def query(hash_value, username=None, password=None, qid=None, args=None):
 
 
 def random_number(hash_value, fncid):
+    """
     raw_data = {"fncid": fncid, "sysyear": "103", "syssms":
                 "1", "online": "okey", "loginid": "1102108130"}
     r = sd[hash_value].post(fnc_url, data=raw_data)
@@ -77,7 +84,14 @@ def random_number(hash_value, fncid):
     lsr = root.xpath("//input")[-1].values()[-1]
 
     return lsr
+    """
+    payload = {}
+    r = sd[hash_value].post(fnc_url, data={"fncid": fncid})
+    res = bs(r.text.replace('<input', '</input><input'))
+    for i in res.findAll('input'):
+        payload[i['name']] = i['value']
 
+    return payload
 
 if __name__ == "__main__":
     pass
